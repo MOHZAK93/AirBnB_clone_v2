c0b261d2c17670b20c15733b447c8faea1013552
MOhammed Zakaria <kokoriquaye@gmail.com>
2023-05-21 12:38:07 +0000

    flask web app

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 @app.route('/', strict_slashes=False)
 def hello():
     """Start a basic Flask web Application"""
-    return 'Hello HBNB'
+    return 'Hello HBNB!'
 
 
 if __name__ == '__main__':
