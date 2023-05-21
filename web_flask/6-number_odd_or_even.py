130dafa9b14459558068a45db47cecc475e3e366
MOhammed Zakaria <kokoriquaye@gmail.com>
2023-05-21 15:16:06 +0000

    odd or even

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""
+Starts a Flask web app
+"""
+from flask import Flask
+from flask import render_template
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """Start a basic Flask web app"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Adds a specific route /hbnb"""
+    return 'HBNB'
+
+
+@app.route('/c/<string:text>', strict_slashes=False)
+def text(text=None):
+    """Dynamic inputed text: replace _ for space and show text"""
+    return 'C {}'.format(text.replace('_', ' '))
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<string:text>', strict_slashes=False)
+def python_text(text='is_cool'):
+    """Dynamic input text: replace '_' for ' ' and show text"""
+    return 'Python {}'.format(text.replace('_', ' '))
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def only_digits_dynamic(n=None):
+    """Dynamic input integer"""
+    return '{} is a number'.format(n)
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def first_template(n=None):
+    """Display a HTML page only if n is an integer"""
+    return render_template('5-number.html', n=n)
+
+
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def conditional_templating(n=None):
+    """Display an HTML page only if n is odd or even"""
+    return render_template('6-number_odd_or_even.html', n=n)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
