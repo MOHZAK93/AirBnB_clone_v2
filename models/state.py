82fed163f0f142ccc1af328441ab34be14cfc808
MOhammed Zakaria <kokoriquaye@gmail.com>
2023-05-25 20:37:08 +0000

    fix

--- models/state.py
+++ models/state.py
@@ -4,24 +4,24 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, Integer, DateTime, ForeignKey
 from sqlalchemy.orm import relationship
 from models.city import City
-import models
 
 
 class State(BaseModel, Base):
     """ State class """
-    if models.storage_type == 'db':
-        __tablename__ = "states"
-        name = Column(String(128), nullable=False)
-        cities = relationship('City', backref='state')
-    else:
-        name = ''
-    if models.storage_type != 'db':
-        @property
-        def cities(self):
-            """Getter attribute cities that returns the list of Cities"""
-            related_cities = []
-            cities = models.storage.all(City)
-            for city in cities.values():
-                if self.id == city.state_id:
-                    related_cities.append(city)
-            return related_cities
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    cities = relationship(
+            'City',
+            backref='state',
+            cascade="all, delete-orphan")
+
+    @property
+    def cities(self):
+        """Getter attribute cities that returns the list of Cities"""
+        from models import storage
+        related_cities = []
+        cities = storage.all(City)
+        for city in cities.values():
+            if self.id == city.state_id:
+                related_cities.append(city)
+        return related_cities
